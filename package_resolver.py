332c9334e94f7ffc0214af07df1a6b447d17d45b
Michael <81708672+litvinovmitch11@users.noreply.github.com>
2023-04-16 11:07:04 +0300

    Update package_resolver.py

--- package_resolver.py
+++ package_resolver.py
@@ -1,80 +1,25 @@
-from pprint import pprint
-from parser import Parser
+from manager import Manager
+import argparse
 
-filename = 'test.txt'
-parser = Parser(filename)
-parser.make_tree()
-tree = parser.get_tree()
+parser = argparse.ArgumentParser(
+    prog='PackageResolver',
+    description='Find dependencies in package versions')
 
-dependencies = {}
-used = {}
+parser.add_argument('filename', type=str, help='Name of file with dependencies')
+parser.add_argument('package_name', type=str, help='Package name for which you want to get the latest version')
+parser.add_argument('-v', '--package_version', type=int,
+                    help='Possible set of all transitive dependencies for given package name and version')
 
+args = parser.parse_args()
 
-def find_conflicts(package, version):
-    global used
-    if (package, version) in used:
-        return used[(package, version)]
-
-    global tree
-    global dependencies
-
-    if len(tree[package][version]) == 0:
-        used[(package, version)] = True
-        return used[(package, version)]
-
-    for name in tree[package][version]:
-        if name not in dependencies:
-            dependencies[name] = tree[package][version][name]
-        else:
-            dependencies[name] &= tree[package][version][name]
-
-        if len(dependencies[name]) == 0:
-            used[(package, version)] = False
-            return used[(package, version)]
-
-    used[(package, version)] = all([any([find_conflicts(n, v) for v in dependencies[n]]) for n in dependencies])
-    return used[(package, version)]
-
-
-find_conflicts('a', 1)
-print(dependencies)
-from pprint import pprint
-from parser import Parser
-
-filename = 'test.txt'
-parser = Parser(filename)
-parser.make_tree()
-tree = parser.get_tree()
-
-dependencies = {}
-used = {}
-
-
-def find_conflicts(package, version):
-    global used
-    if (package, version) in used:
-        return used[(package, version)]
-
-    global tree
-    global dependencies
-
-    if len(tree[package][version]) == 0:
-        used[(package, version)] = True
-        return used[(package, version)]
-
-    for name in tree[package][version]:
-        if name not in dependencies:
-            dependencies[name] = tree[package][version][name]
-        else:
-            dependencies[name] &= tree[package][version][name]
-
-        if len(dependencies[name]) == 0:
-            used[(package, version)] = False
-            return used[(package, version)]
-
-    used[(package, version)] = all([any([find_conflicts(n, v) for v in dependencies[n]]) for n in dependencies])
-    return used[(package, version)]
-
-
-find_conflicts('a', 1)
-print(dependencies)
+manager = Manager(args.filename)
+if args.package_version:
+    if manager.set_versions(args.package_name, args.package_version):
+        print('Available combination of versions:')
+        versions = manager.get_versions()
+        for name in versions:
+            print(f'{name}: {versions[name]}')
+    else:
+        print('No available combination of versions')
+else:
+    print(f'Last version of package "{args.package_name}" is {manager.get_last_version(args.package_name)}')
